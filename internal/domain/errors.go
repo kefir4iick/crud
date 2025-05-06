9361b3dc1aaa02eb24376884f5b4abffe8e48cdb
kefir4iick <lizardm17@icloud.com>
2025-05-06 23:54:55 +0300

    fix bug

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -3,5 +3,9 @@ package domain
 import "errors"
 
 var (
-	ErrCarNotFound = errors.New("car not found")
+	ErrCarNotFound      = errors.New("car not found")
+	ErrDuplicateCarID   = errors.New("car with this ID already exists")
+	ErrInvalidInput     = errors.New("invalid input")
+	ErrInvalidLimit     = errors.New("invalid limit value")
+	ErrInvalidOffset    = errors.New("invalid offset value")
 )
